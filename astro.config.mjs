6af91bf913da5e986524399bfcada8932a083174
DANO0007 <dann_96__@hotmail.com>
2025-06-26 14:20:00 -0600

    chore: change site URL in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://jalieza.com",
+  site: "https://jalieza.com.mx",
   base: "/",
   trailingSlash: "ignore",
   integrations: [],
